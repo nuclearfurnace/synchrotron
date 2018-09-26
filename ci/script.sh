5a93f10c8380c1b591467024d254c2ab0c02f32d
Toby Lawrence <tobz@users.noreply.github.com>
2018-09-25 21:36:59 -0400

    Remove Windows tests. (#9)
    Since we're planning on utilizing signals to do online reconfiguration,
    this probably isn't going to work for Windows.  Windows just seems to
    have less-good tooling around sending signals to processes, etc.

--- ci/script.sh
+++ ci/script.sh
@@ -1,18 +1,14 @@
-# This script takes care of testing your crate
-
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
-    cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cross test --target $TARGET
-    cross test --target $TARGET --release
+    scripts/run-integration-tests.sh
 }
 
 # we don't run the "test phase" when doing deploys
